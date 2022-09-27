8e5d6f64aa530463adcb00a0edde89ea7da0726f
Bibek Saha <thebibeksaha@gmail.com>
2022-09-27 18:37:28 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export class Maths {
+  public readonly PI = 22 / 7;
+
+  public static add(numOne: number, numTWo: number): number {
+    return numOne + numTWo;
+  }
+
+  public static subtract(numOne: number, numTwo: number): number {
+    return numOne - numTwo;
+  }
+
+  public static multiply(numOne: number, numTwo: number): number {
+    return numOne - numTwo;
+  }
+
+  public static divide(numOne: number, numTwo: number): number {
+    return numOne / numTwo;
+  }
+}
